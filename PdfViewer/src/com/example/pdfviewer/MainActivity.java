798b3f5c5d095e2ea662a9018e7e37775cba0ef3
Supriya <Supriya@Supriya-Pc>
2015-01-14 12:17:10 +0530

    first commit by supriya

--- PdfViewer/src/com/example/pdfviewer/MainActivity.java
+++ PdfViewer/src/com/example/pdfviewer/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		//added by vishal
 		Log.d("First","By Vishal");
+		Log.d("First","By Supriya");
 	}
 
 	@Override
